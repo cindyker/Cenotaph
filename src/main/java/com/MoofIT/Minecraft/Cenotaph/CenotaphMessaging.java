0d670b1aa93b058696d2d1fe086258a4a448985e
Llm Dl <LlmDlio@gmail.com>
2019-08-29 10:24:26 -0500

    Forgot to commit CenotaphMessaging.java

---
+++ src/main/java/com/MoofIT/Minecraft/Cenotaph/CenotaphMessaging.java
@@ -0,0 +1,48 @@
+package com.MoofIT.Minecraft.Cenotaph;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class CenotaphMessaging {
+
+	
+	/**
+	 * Sends a prefixed message to a player
+	 * [Cenotaph] - in Gold
+	 * Message in White
+	 *  
+	 * @param player
+	 * @param message
+	 */
+	public static void sendPrefixedPlayerMessage(Player player, String message) {
+		player.sendMessage(ChatColor.GOLD + "[Cenotaph] " + ChatColor.WHITE + message);
+	}
+	
+	public static String centimeMsg(TombBlock tBlock) {
+		String msg = null;
+		if (CenotaphSettings.securityEnable()) { 
+			msg = ChatColor.YELLOW + "Security: " + ChatColor.WHITE + (!tBlock.isSecured() ? "Unsecured " : "Secured"); 	
+			if (tBlock.isSecured())
+				msg += ChatColor.YELLOW + " SecTime: " + ChatColor.WHITE + (CenotaphSettings.securityRemove() ? CenotaphUtil.convertTime(tBlock.securityTimeLeft()) + " " : "Inf ");
+		}
+		if (CenotaphSettings.cenotaphRemove() && !CenotaphSettings.keepUntilEmpty()) 
+			msg += ChatColor.YELLOW + "BreakTime: " + ChatColor.WHITE + (CenotaphSettings.cenotaphRemove() ? CenotaphUtil.convertTime(tBlock.removalTimeLeft()) : "Inf") + " ";
+		if (CenotaphSettings.removeWhenEmpty() || CenotaphSettings.keepUntilEmpty()) {
+			msg += ChatColor.YELLOW + "BreakOverride: " + ChatColor.WHITE;
+			if (CenotaphSettings.removeWhenEmpty()) msg += "Break on empty";
+			if (CenotaphSettings.removeWhenEmpty() && CenotaphSettings.keepUntilEmpty()) msg += " & ";
+			if (CenotaphSettings.keepUntilEmpty()) msg += "Keep until empty";
+		}
+		return msg;
+	}
+	
+	public static void sendActionBarPlayerMessage(Player player, String message) {
+		if (!Cenotaph.isSpigot)
+			sendPrefixedPlayerMessage(player, message);
+		else 
+			player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+	}
+}
